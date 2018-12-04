24aa5f9e0373830d41f14ab2e990b16b01d1c939
Steffen Haas <haas@informatik.uni-hamburg.de>
2018-12-04 15:49:43 +0100

    Adding resources

---
+++ honeygrove/resources/email_resources/database.py
@@ -0,0 +1,104 @@
+mails = [
+
+[
+    "Sent",
+    {
+        "Date": "Sun, 1 Jul 2018 23:37:04 +0000",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Thunderbird/52.8.0",
+        "MIME-Version": "1.0",
+        "From": "Bär <honig@hier.de>",
+        "To": "somewhere@nowhere.com",
+        "Subject": "Re: Always is never",
+    },
+    "...ut eu ut laboris esse fugiat voluptate amet ex occaecat eu sint \r\nincididunt consequat aliqua voluptate aliqua veniam in ex\r\n\r\nOn 08/11/2017 10:47 AM, somewhere@nowhere.com wrote:\r\n> Culpa dolore consectetur qui sed aliquip mollit dolor nostrud...\r\n>"
+],
+
+[
+    "INBOX",
+    {
+        "Content-Type": "text/plain; charset=utf-8",
+        "Message-Id": "<20160defewf.C63DFE@grosbeek.riseup.net>",
+        "Date": "Fri, 11 Aug 2016 12:47:07 +0200",
+        "From": "dev@dev.null",
+        "To": "diese@adres.se",
+        "Subject": "Re: doc | FTP Fehler in FileZilla (#3)",
+    },
+    "Der Fehler ist bekannt und jetzt auch als Bug makiert. Wir sitzen momentan in der Gruppe eher an der implementierung von neuen Servicen. Schränkt euch das ganze sehr in eurer arbeit ein? Falls ja, könnte man ja Mittwoch in der Besprechung gucken ob man den Backlog dementsprechend anpasst."
+],
+
+[
+    "INBOX",
+    {
+        "Date": "Fri, 11 Aug 2017 12:47:07 +0200",
+        "From": "somewhere@nowhere.com",
+        "To": "diese@adres.se",
+        "Subject": "Always is never",
+    },
+    "Culpa dolore consectetur qui sed aliquip mollit dolor nostrud..."
+],
+
+
+[
+    "INBOX",
+    {
+        "Date": "Sun, 1 Jul 2018 23:37:04 +0000",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Thunderbird/52.8.0",
+        "MIME-Version": "1.0",
+        "From": "Bär <honig@hier.de>",
+        "To": "somewhere@nowhere.com",
+        "Subject": "Re: Always is never",
+    },
+    "...ut eu ut laboris esse fugiat voluptate amet ex occaecat eu sint \r\nincididunt consequat aliqua voluptate aliqua veniam in ex\r\n\r\nOn 08/11/2017 10:47 AM, somewhere@nowhere.com wrote:\r\n> Culpa dolore consectetur qui sed aliquip mollit dolor nostrud...\r\n>"
+],
+
+[
+    "Trash",
+    {
+        "Date": "Fri, 11 Aug 2018 12:47:07 +0200",
+        "From": "potus@whitehouse.gov",
+        "To": "diese@adres.se",
+        "Subject": "Make HoneyGrove great again!",
+    },
+    "rhabarber rhabarber rhabarber the greatest rhabarber ever rhabarber rhabarber"
+],
+
+[
+    "Sent",
+    {
+        "Date": "Sun, 1 Jul 2018 23:37:04 +0000",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Thunderbird/52.8.0",
+        "MIME-Version": "1.0",
+        "From": "Bär <honig@hier.de>",
+        "To": "somewhere@nowhere.com",
+        "Subject": "Re: Always is never",
+    },
+    "...ut eu ut laboris esse fugiat voluptate amet ex occaecat eu sint \r\nincididunt consequat aliqua voluptate aliqua veniam in ex\r\n\r\nOn 08/11/2017 10:47 AM, somewhere@nowhere.com wrote:\r\n> Culpa dolore consectetur qui sed aliquip mollit dolor nostrud...\r\n>"
+],
+
+[
+    "Drafts",
+    {
+        "Date": "Sun, 1 Jul 2018 23:37:04 +0000",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Thunderbird/52.8.0",
+        "MIME-Version": "1.0",
+        "From": "Bär <honig@hier.de>",
+        "To": "somewhere@nowhere.com",
+        "Subject": "Re: Always is never",
+    },
+    "...ut eu ut laboris esse fugiat voluptate amet ex occaecat eu sint \r\nincididunt consequat aliqua voluptate aliqua veniam in ex\r\n\r\nOn 08/11/2017 10:47 AM, somewhere@nowhere.com wrote:\r\n> Culpa dolore consectetur qui sed aliquip mollit dolor nostrud...\r\n>"
+],
+
+[
+    "Bullshit",
+    {
+        "Date": "Sun, 1 Jul 2018 23:37:04 +0000",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Thunderbird/52.8.0",
+        "MIME-Version": "1.0",
+        "From": "Bär <honig@hier.de>",
+        "To": "somewhere@nowhere.com",
+        "Subject": "Re: Always is never",
+    },
+    "...ut eu ut laboris esse fugiat voluptate amet ex occaecat eu sint \r\nincididunt consequat aliqua voluptate aliqua veniam in ex\r\n\r\nOn 08/11/2017 10:47 AM, somewhere@nowhere.com wrote:\r\n> Culpa dolore consectetur qui sed aliquip mollit dolor nostrud...\r\n>"
+],
+
+]
