9f35490cc1b9582e61c6cd16553f4cf775f8e4ea
caleron <caleron@users.noreply.github.com>
2018-12-09 17:51:56 +0100

    Update runEKStack.sh

--- incidentmonitoring/runEKStack.sh
+++ incidentmonitoring/runEKStack.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+   echo "Starting the EK stack requires root permissions"
+   exit 1
+fi
+
 echo "Rising the maximum memory that virtual machines are allowed to map"
 sudo sysctl -w vm.max_map_count=262144
 
