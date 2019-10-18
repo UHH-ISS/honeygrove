b8c41163274a842982cdb236d39ff71b2bfd497e
Florian Wilkens <wilkens@informatik.uni-hamburg.de>
2019-10-18 11:12:44 +0200

    [REL] Release v0.5.2

--- honeygrove/log.py
+++ honeygrove/log.py
@@ -13,7 +13,7 @@ if Config.use_geoip:
 ECS_SERVICE = {'id': sha256(str(Config.HPID).encode('utf-8')).hexdigest(),
                'name': str(Config.HPID).lower(),
                'type': 'honeygrove',
-               'version': '0.5.1'
+               'version': '0.5.2'
                }
 
 if Config.use_geoip:
